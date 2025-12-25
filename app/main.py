c3640b02e1803375e29895ac7d46ac95ef60fbf6
RozzenRose <RozzenRose@gmail.com>
2025-12-25 14:41:50 +0100

    rate limit implemented

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers.auth import router as auth_router
 from app.routers.orders import router as order_router
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
 
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
 # Настройка CORS
@@ -22,7 +30,8 @@ app.add_middleware(
 
 
 @app.get("/")
-async def root():
+@limiter.limit("1/minute")
+async def root(request: Request):
     return {"message": "Hello, world!"}
 
 
